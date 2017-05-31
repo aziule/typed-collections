4bfca46f7ac76812c5b55eec0ddb4addb4e6807d
Will <willyboy_43@hotmail.com>
2017-05-31 10:58:22 +0100

    Test the object collection

---
+++ tests/ObjectCollectionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Aziule\Test\TypedCollection;
+
+use Aziule\TypedCollections\Exception\ClassNotFoundException;
+use Aziule\TypedCollections\Exception\InvalidItemTypeException;
+use Aziule\TypedCollections\ObjectCollection;
+
+class ObjectCollectionTest extends BaseCollectionTest
+{
+    public function test_create_empty_object()
+    {
+        $collection = new ObjectCollection(\stdClass::class);
+        $this->assertCount(0, $collection);
+
+        $this->expectException(ClassNotFoundException::class);
+        new ObjectCollection('Unexisting class');
+    }
+
+    public function test_create_correct_object()
+    {
+        $initialData = [
+            $this->createObject('John'),
+            $this->createObject('Jack'),
+            $this->createObject('Jill'),
+        ];
+
+        $collection = new ObjectCollection(\stdClass::class, $initialData);
+        $this->assertCount(count($initialData), $collection);
+
+        $this->assertCollectionEqualsData($initialData, $collection);
+    }
+
+    public function test_create_incorrect_object()
+    {
+        $initialData = [
+            $this->createObject('John'),
+            $this->createObject('Jack'),
+            $this->createObject('Jill'),
+            'Gerard',
+        ];
+
+        $this->expectException(InvalidItemTypeException::class);
+        new ObjectCollection(\stdClass::class, $initialData);
+    }
+
+    /**
+     * @param mixed $value
+     * @return \stdClass
+     */
+    private function createObject($value) {
+        $object = new \stdClass();
+        $object->property = $value;
+
+        return $object;
+    }
+}
